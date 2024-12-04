8b269c1ea4c897fd9249a441ffa23f0a2aa2545c
amiceli <antoine@miceli.click>
2024-12-04 22:31:26 +0100

    refactor: use starlight for vitest-cucumber-doc website (#16)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,114 @@
-import mdx from "@astrojs/mdx"
-// import pagefind from "astro-pagefind"
+import starlight from "@astrojs/starlight"
 import { defineConfig } from "astro/config"
 
 export default defineConfig({
-    // integrations: [mdx(), pagefind()],
-    integrations: [mdx()],
+    integrations: [
+        starlight({
+            title: "vitest-cucumber",
+            logo: {
+                src: "src/assets/logo.png",
+            },
+            social: {
+                github: "https://github.com/amiceli/vitest-cucumber",
+            },
+            sidebar: [
+                {
+                    label: "Get started",
+                    items: [
+                        {
+                            label: "Presentation",
+                            slug: "get-started/presentation",
+                        },
+                        {
+                            label: "Installation",
+                            slug: "get-started/install",
+                        },
+                        {
+                            label: "Configuration",
+                            slug: "get-started/configuration",
+                        },
+                    ],
+                },
+                {
+                    label: "Vitest plugin",
+                    items: [
+                        {
+                            label: "Setup",
+                            slug: "plugin/setup",
+                        },
+                    ],
+                },
+                {
+                    label: "Features",
+                    items: [
+                        {
+                            label: "Gherkin",
+                            slug: "features/gherkin",
+                        },
+                        {
+                            label: "Scenario",
+                            slug: "features/scenario",
+                        },
+                        {
+                            label: "Scenario Outline",
+                            slug: "features/scenario-outline",
+                        },
+                        {
+                            label: "Background",
+                            slug: "features/background",
+                        },
+                        {
+                            label: "Rule",
+                            slug: "features/rule",
+                        },
+                        {
+                            label: "Hooks",
+                            slug: "features/hooks",
+                        },
+                        {
+                            label: "Tags",
+                            slug: "features/gherkin-tags",
+                        },
+                        {
+                            label: "Sequentially / Async",
+                            slug: "features/sequentially-and-async",
+                        },
+                        {
+                            label: "Step expressions",
+                            slug: "features/step-expression",
+                        },
+                        {
+                            label: "DocStrings",
+                            slug: "features/doc-strings",
+                        },
+                        {
+                            label: "Date tables",
+                            slug: "features/data-tables",
+                        },
+
+                        {
+                            label: "Spoken language",
+                            slug: "features/spoken-languages",
+                        },
+                    ],
+                },
+            ],
+            customCss: ["./src/styles/custom.css"],
+            head: [
+                {
+                    tag: "script",
+                    attrs: {
+                        src: "https://plausible.volpe.xyz/script.js",
+                        "data-website-id":
+                            "5e2eb5c2-a935-4df2-b9fb-9ad82dd0ec8a",
+                        "data-domains": "vitest-cucumber.miceli.click",
+                        defer: true,
+                    },
+                },
+            ],
+        }),
+    ],
     devToolbar: {
         enabled: false,
-    },
-    // build: {
-    //     format: "file",
-    // },
-    markdown: {
-        shikiConfig: {
-            theme: "github-dark-default",
-            langs: ["gherkin"],
-        },
     },
 })
